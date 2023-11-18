50d298d826636fd3338b5d38bc998636a9d5a0fd
ovaiskhanday <ovaiskhanday927@gmail.com>
2023-11-18 13:16:08 +0530

    Remove service.dart dependency from location_service.dart

--- lib/logic/location_service.dart
+++ lib/logic/location_service.dart
@@ -1,13 +1,7 @@
-import 'package:flutter/services.dart';
 import 'package:location/location.dart';
 
 class LocationService {
   Location location = Location();
-
-  Future<PermissionStatus> requestPermission() async {
-    final permission = await location.requestPermission();
-    return permission;
-  }
 
   Future<bool> _requestService({int limit = 20}) async {
     for (var i = 0; i < limit; i++) {
@@ -21,18 +15,14 @@ class LocationService {
   }
 
   Future<LocationData> getCurrentLocation() async {
-    try {
-      final serviceEnabled = await _requestService();
-      if (!serviceEnabled) {
-        if (await location.requestService() == false) {
-          throw Exception('GPS service not enabled');
-        }
+    final serviceEnabled = await _requestService();
+    if (!serviceEnabled) {
+      if (await location.requestService() == false) {
+        throw Exception('GPS service not enabled');
       }
-    } catch (e) {
-      throw Exception('Error in getting location service');
     }
 
-    final permissionGranted = await requestPermission();
+    final permissionGranted = await location.requestPermission();
     if (permissionGranted == PermissionStatus.denied || permissionGranted == PermissionStatus.deniedForever) {
       throw Exception("Location Permission not granted");
     }
